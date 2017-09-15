f2068be80869a2e9a6d854b36c9d78b0692fc88e
Andy Sloane <andy@a1k0n.net>
2017-09-15 16:15:48 -0700

    fix jstest

--- src/hw/input/jstest.cc
+++ src/hw/input/jstest.cc
@@ -3,6 +3,32 @@
 
 #include "hw/input/js.h"
 
+#include <stdio.h>
+#include <unistd.h>
+
+#include "hw/input/js.h"
+
+class TestInputReceiver : public InputReceiver {
+  void OnDPadPress(char direction) {
+    printf("DPadPress   %c\n", direction);
+  }
+
+  void OnDPadRelease(char direction) {
+    printf("DPadRelease %c\n", direction);
+  }
+
+  void OnButtonPress(char button) {
+    printf("ButtonPress   %d\n", button);
+  }
+  void OnButtonRelease(char button) {
+    printf("ButtonRelease %d\n", button);
+  }
+
+  void OnAxisMove(int axis, int16_t value) {
+    printf("Axis %d %d\n", axis, value);
+  }
+};
+
 int main() {
   JoystickInput js;
 
@@ -10,13 +36,12 @@ int main() {
     return 1;
   }
 
+  TestInputReceiver tir;
+
   for (;;) {
     int t = 0, s = 0;
     uint16_t b = 0;
-    if (js.ReadInput(&t, &s, &b)) {
-      printf("%6d %6d %4x\r", t, s, b);
-      fflush(stdout);
-    }
+    js.ReadInput(&tir);
     usleep(10000);
   }
 }
