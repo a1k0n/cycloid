c3a9681c95d86cded3559e4f293a5f69dda2a938
Andy Sloane <andy@a1k0n.net>
2018-11-07 06:49:31 -0800

    update lookup table (y vanishing point changed)

--- design/coneslam/params.py
+++ design/coneslam/params.py
@@ -1,6 +1,6 @@
 # parameters for detection
-vpy = 234  # y vanishing point
+vpy = 224  # y vanishing point
 turn_slope = 15
 #turn_slope = 0
-threshold = 450
+threshold = 400
 bandheight = 8  # height of vertical band of pixels to look at
