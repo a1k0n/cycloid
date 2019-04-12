746f68bdecee923ac82a508ee10047bd2caf924d
Andy Sloane <andy@a1k0n.net>
2019-04-12 07:15:35 -0700

    add equirectangular mode

--- design/coneslam/recordreader.py
+++ design/coneslam/recordreader.py
@@ -62,6 +62,9 @@ def parse_args():
 
     p.add_argument('-o', '--output', type=str, default=None,
             help='Video file to save to')
+    p.add_argument('-r', '--remap', dest='remap', action='store_true',
+                   help='remap frame to equirectangular projection')
+    p.add_argument('-e', '--exposure', type=float, default=1.0, help='exposure compensation')
     p.add_argument('recordfile', type=str, help='Recording file from car')
     return p.parse_args()
 
@@ -72,11 +75,39 @@ if __name__ == '__main__':
 
     args = parse_args()
 
-    f = open(args.recordfile)
+    remap = None
+
+    if args.remap:
+        camera_matrix = np.load("../../tools/camcal/camera_matrix.npy")
+        dist_coeffs = np.load("../../tools/camcal/dist_coeffs.npy")
+        # downconvert camera matrix from 2592x1944
+        fx, fy, _ = np.diag(camera_matrix) / 4.05
+        cx, cy = camera_matrix[:2, 2] / 4.05
+        print('camera calibration: fx', fx, 'fy', fy, 'cx', cx, 'cy', cy, 'k1', dist_coeffs[0])
+        def ang2pix(lat, lon):
+            theta = np.pi/2 - lat
+            theta = theta*(1 + dist_coeffs[0]*theta**2)
+            x = (fx*np.outer(theta, np.cos(-lon)) + cx)
+            y = (fy*np.outer(theta, np.sin(-lon)) + cy)
+            return np.stack([x, y]).transpose(1, 2, 0).astype(np.float32)
+
+        f32map = ang2pix(np.linspace(np.pi/2, -np.pi/2, 1024),
+                         np.linspace(-np.pi, np.pi, 2048))
+        remap = cv2.convertMaps(f32map, None, cv2.CV_16SC2)
+
+
+    f = open(args.recordfile, "rb")
     vidout = None
     if args.output: 
+        siz = (640, 480)
+        if remap is not None:
+            siz = (remap[0].shape[1], remap[0].shape[0])
         vidout = cv2.VideoWriter(args.output, cv2.VideoWriter_fourcc(
-            'X', '2', '6', '4'), 28.8, (640, 480), True)
+#            'M', 'J', 'P', 'G'), 30, siz, True)
+            'X', '2', '6', '4'), 30, siz, True)
+        if vidout is None:
+            sys.exit(1)
+
     t0 = None
     n = 0
     variance = 0.0 
@@ -90,6 +121,10 @@ if __name__ == '__main__':
         tstamp, carstate, particles, controldata, conesx, frame = data
         bgr = cv2.cvtColor(
             frame.reshape((-1, imgsiz[0])), cv2.COLOR_YUV2BGR_I420)
+        bgr = np.clip(bgr * args.exposure, 0, 255).astype(np.uint8)
+        if remap is not None:
+            bgr = cv2.remap(bgr, remap[0], remap[1], cv2.INTER_LINEAR)
+            bgr[bgr.shape[0]//2, :, :] = 255
         if t0 is None:
             t0 = tstamp
             tstamp_last = tstamp 
@@ -105,7 +140,8 @@ if __name__ == '__main__':
         annotate.draw_steering(bgr, carstate[1], carstate[4])
         annotate.draw_speed(bgr, tstamp, carstate[5], carstate[6])
         annotate.draw_throttle(bgr, carstate[0])
-        cv2.imshow("camera", bgr)
+        if args.interactive:
+            cv2.imshow("camera", bgr)
         if vidout is not None:
             vidout.write(bgr)
         print(carstate)
@@ -116,7 +152,8 @@ if __name__ == '__main__':
         inview = ((partxy[:, 0] >= 0) & (partxy[:, 0] < 450) &
                   (partxy[:, 1] <= 0) & (partxy[:, 1] > -240))
         partview[(-partxy[inview, 1], partxy[inview, 0])] = 255
-        cv2.imshow("particles", partview)
+        if args.interactive:
+            cv2.imshow("particles", partview)
 
         for x in conesx:
             cv2.line(bgr, (x, params.vpy), (x, params.vpy + params.bandheight),
@@ -124,9 +161,9 @@ if __name__ == '__main__':
 
         (x, y, theta, vf, vr, w, ierr_v, ierr_w, delta,
          target_k, target_v, target_w, ye, psie, k, bw_w, bw_v) = controldata
-        print 'xy', x, y, 'theta', theta, 'ye', ye, 'psie', psie, 'k', k
-        print 'target_k', target_k, 'target_v', target_v, target_k*target_v
-        print 'target_w', target_w, 'w', w
+        print('xy', x, y, 'theta', theta, 'ye', ye, 'psie', psie, 'k', k)
+        print('target_k', target_k, 'target_v', target_v, target_k*target_v)
+        print('target_w', target_w, 'w', w)
 
         if args.interactive:
             k = cv2.waitKey()
