b6622bd5402cc9646194bad0a2788aee5e23846c
Andy Sloane <andy@a1k0n.net>
2019-04-15 21:54:00 -0700

    fisheye WIP

---
+++ src/coneslam/fisheye.cc
@@ -0,0 +1,25 @@
+#include <math.h>
+#include "coneslam/fisheye.h"
+
+// how much visualizer shows under the horizon vs above it (out of 56 pixels)
+static const int vis_y0 = 10;
+
+Fisheye::Fisheye(float w, float h, float fx, float fy, float cx, float cy,
+                 float k1) {
+  float theta = M_PI/2;
+  theta = theta*(1 + k1*theta*theta);  // apply distortion
+
+  int npix = rint(M_PI*2*fx*theta);  // get circumference of horizon in pixels
+  lut_linear_ = new uint32_t[npix];
+
+  // also build a remap table just for visualization on a display
+  // 320x56 front, 320x56 rear
+  lut_remap_ = new uint32_t[320*112];
+  for (int j = 0; j < 56; j++) {
+    // get latitude angle from pixel location
+    float lat = M_PI/2 - (j - vis_y0) / fx;
+    for (int i = 0; i < 320; i++) {
+    }
+  }
+}
+
---
+++ src/coneslam/fisheye.h
@@ -0,0 +1,30 @@
+#ifndef CONESLAM_FISHEYE_H_
+#define CONESLAM_FISHEYE_H_
+
+#include <stdint.h>
+
+// Camera calibration for fisheye lenses;
+// generates lookup tables and reprojects images
+
+class Fisheye {
+ public:
+  // construct fisheye lens for image of given size (w, h),
+  // focal length (fx, fy),
+  // optical center (cx, cy),
+  // and distortion k1
+  Fisheye(float w, float h, float fx, float fy, float cx, float cy, float k1);
+
+  ~Fisheye() {
+    delete[] lut_linear_;
+    delete[] lut_remap_;
+  }
+
+ private:
+  uint32_t *lut_linear_;
+  int lut_xres_;  // horizontal resolution of linear LUT
+
+  uint32_t *lut_remap_;
+  int remap_w_, remap_h_;  // remap size
+};
+
+#endif  // CONESLAM_FISHEYE_H_
