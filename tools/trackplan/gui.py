7d4e548d69ee67fc68477a8416bfac0e9a68b79b
Andy Sloane <andy@a1k0n.net>
2019-04-19 17:13:51 -0700

    gui wip

--- tools/trackplan/gui.py
+++ tools/trackplan/gui.py
@@ -2,14 +2,38 @@
 from __future__ import print_function
 
 import cv2
+import numpy as np
+import pickle
 import glfw
 import OpenGL.GL as gl
 import imgui
 from imgui.integrations.glfw import GlfwRenderer
 
 
+def load_texture(im):
+    # gl.glEnable(gl.GL_TEXTURE_2D)
+    texid = gl.glGenTextures(1)
+    gl.glBindTexture(gl.GL_TEXTURE_2D, texid)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D,
+                       gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D,
+                       gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA,
+                    im.shape[1], im.shape[0], 0,
+                    gl.GL_BGR, gl.GL_UNSIGNED_BYTE, im)
+    return texid
+
+
+def unload_texture(texid):
+    gl.glDeleteTextures([texid])
+
+
 def imagepicker(name, im, imtex, ptlist):
     imgui.begin(name)
+    changed = False
+    if imgui.button("delete last"):
+        ptlist = ptlist[:-1]
+        changed = True
     w, h = imgui.get_window_size()
     h = im.shape[0] * w / im.shape[1]
     imgui.image_button(imtex, w, h, frame_padding=0)
@@ -21,7 +45,7 @@ def imagepicker(name, im, imtex, ptlist):
         u = p[0] * w / im.shape[1] + rectmin[0]
         v = p[1] * w / im.shape[1] + rectmin[1]
         dl.add_rect_filled(u - 4, v - 4, u + 4, v + 4,
-                    imgui.get_color_u32_rgba(1, 0.8, 0.1, 1), 4)
+                           imgui.get_color_u32_rgba(1, 0.8, 0.1, 1), 4)
         dl.add_rect(u - 4.5, v - 4.5, u + 4.5, v + 4.5,
                     imgui.get_color_u32_rgba(0, 0, 0, 1), 5)
 
@@ -48,13 +72,99 @@ def imagepicker(name, im, imtex, ptlist):
             mxy = imgui.get_mouse_pos()
             u = (mxy[0] - rectmin[0]) * im.shape[1] / w
             v = (mxy[1] - rectmin[1]) * im.shape[1] / w
-            print("added", name, "point", u, v)
             ptlist.append((u, v))
+            changed = True
 
     imgui.end()
+    return ptlist, changed
 
 
-def main(im1, im2):
+def Rmatrix(N):
+    R2 = np.eye(3)
+    R2[2] = N.reshape(-1)
+    R2[0] = np.cross(R2[0], R2[2])
+    R2[0] /= np.linalg.norm(R2[0])
+    R2[1] = np.cross(R2[2], R2[0])
+    R2[1] /= np.linalg.norm(R2[1])
+    return R2
+
+
+class RemapWindow:
+    def __init__(self, im1, K):
+        self.remappedtex = None
+        self.remappedim = None
+        self.offxy = (0, 0)
+        self.im1 = im1
+        self.K = K
+        self.editmode = 'cone'
+        self.cones = []
+        self.turns = []
+        self.scalerefpts = []
+        self.scaleref = 1.0
+
+    def render_turns(self):
+        # TODO
+        pass
+
+    def render(self, ptlist1, ptlist2, changed):
+        imgui.begin("birdseye")
+        if len(ptlist1) < 4:
+            imgui.text("please add at least 4 points to each image")
+            imgui.end()
+            return
+        if len(ptlist1) != len(ptlist2):
+            imgui.text("please add the same number of points to each image")
+            imgui.end()
+            return
+
+        offsetchanged, self.offxy = imgui.slider_float2(
+            "offset xy", *self.offxy, min_value=-10, max_value=10)
+
+        for i, mode in enumerate(["cone", "turn", "scaleref"]):
+            if i != 0:
+                imgui.same_line()
+            if imgui.radio_button(mode, self.editmode == mode):
+                self.editmode = mode
+                print('editmode', mode)
+
+        imgui.same_line()
+        if imgui.button("delete last"):
+            if self.editmode == "cone":
+                self.cones = self.cones[:-1]
+            if self.editmode == "turn":
+                self.turns = self.turns[:-1]
+
+        _, self.scaleref = imgui.input_float("scale reference dist (m)", self.scaleref)
+
+        # i don't think we actually need this button now
+        if imgui.button("recompute mapping") or changed or offsetchanged:
+            H, _ = cv2.findHomography(np.float32(ptlist1), np.float32(ptlist2))
+            n, R, t, N = cv2.decomposeHomographyMat(H, self.K)
+            # refpts = np.hstack([ptlist1, np.ones((len(ptlist1), 1))])
+            n = N[np.argmin(np.array(N)[:, 1, 0])]
+            R2 = Rmatrix(n)
+            scale = 200
+            size = 1000
+            M2 = np.float32([
+                [-scale, 0, size/2 - scale*self.offxy[0]],
+                [0, scale, size/2 - scale*self.offxy[1]],
+                [0, 0, 1]
+            ])
+            MM = np.dot(M2, np.dot(R2, np.linalg.inv(self.K)))
+            self.remappedim = cv2.warpPerspective(self.im1, MM, (size, size))
+            if self.remappedtex is not None:
+                unload_texture(self.remappedtex)
+            self.remappedtex = load_texture(self.remappedim)
+
+        if self.remappedtex is not None:
+            w, h = imgui.get_window_size()
+            h = self.remappedim.shape[0] * w / self.remappedim.shape[1]
+            imgui.image(self.remappedtex, w, h)
+
+        imgui.end()
+
+
+def main(im1, im2, K):
     im1 = cv2.imread(im1)
     im2 = cv2.imread(im2)
 
@@ -68,23 +178,50 @@ def main(im1, im2):
     ptlist1 = []
     ptlist2 = []
 
+    rw = RemapWindow(im1, K)
+
+    status = 'Ready'
     while not glfw.window_should_close(window):
         glfw.poll_events()
         impl.process_inputs()
 
         imgui.new_frame()
 
+        loaded = False
         if imgui.begin_main_menu_bar():
             if imgui.begin_menu("File", True):
-                clicked_quit, selected_quit = imgui.menu_item(
+                clicked_quit, _ = imgui.menu_item(
                     "Quit", 'Cmd+Q', False, True)
                 if clicked_quit:
                     exit(0)
+                load1, _ = imgui.menu_item("Load pts", 'Cmd+L')
+                if load1:
+                    try:
+                        f = open("ptlist.pkl", "rb")
+                        ptlist1, ptlist2 = pickle.load(f)
+                        loaded = True
+                        f.close()
+                    except IOError:
+                        status = 'Unable to load ptlist.pkl'
+                save1, _ = imgui.menu_item("Save pts", 'Cmd+S')
+                if save1:
+                    f = open("ptlist.pkl", "wb")
+                    pickle.dump((ptlist1, ptlist2), f)
+                    f.close()
+                    status = 'Saved point list'
                 imgui.end_menu()
             imgui.end_main_menu_bar()
 
-        imagepicker("image 1", im1, imtex1, ptlist1)
-        imagepicker("image 2", im2, imtex2, ptlist2)
+        ptlist1, changed1 = imagepicker("image 1", im1, imtex1, ptlist1)
+        ptlist2, changed2 = imagepicker("image 2", im2, imtex2, ptlist2)
+
+        rw.render(ptlist1, ptlist2, loaded or changed1 or changed2)
+
+        imgui.set_next_window_position(
+            0, imgui.get_io().display_size[1] - 30)
+        imgui.begin("status", flags=0x7f)
+        imgui.text(status)
+        imgui.end()
 
         gl.glClearColor(0., 0., 0., 1)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
@@ -95,18 +232,6 @@ def main(im1, im2):
 
     impl.shutdown()
     glfw.terminate()
-
-
-def load_texture(im):
-    # gl.glEnable(gl.GL_TEXTURE_2D)
-    texid = gl.glGenTextures(1)
-    gl.glBindTexture(gl.GL_TEXTURE_2D, texid)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
-    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA,
-                    im.shape[1], im.shape[0], 0,
-                    gl.GL_BGR, gl.GL_UNSIGNED_BYTE, im)
-    return texid
 
 
 def impl_glfw_init():
@@ -141,8 +266,11 @@ def impl_glfw_init():
 if __name__ == "__main__":
     import sys
 
+    # need camera calibration matrix
+    K = np.load("../../tools/camcal/flat/camera_matrix.npy")
+
     if len(sys.argv) < 3:
         print("usage:", sys.argv[0], "[img1] [img2]")
         exit(1)
 
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], K)
